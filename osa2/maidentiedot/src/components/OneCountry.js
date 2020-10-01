5dfa352e77144d6c38680542b869559bea19d34a
Mipa <mikko.paivikko@gmail.com>
2020-10-01 10:50:30 +0300

    2.14 tehtävä lopulta toimimaan

--- osa2/maidentiedot/src/components/OneCountry.js
+++ osa2/maidentiedot/src/components/OneCountry.js
@@ -2,27 +2,25 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 const OneCountry = (props) => {
   const { country } = props;
-  const [weather, setWeather] = useState();
+  const [weather, setWeather] = useState([]);
 
-  // const getWeather = () => {
-  //   return weather.data.location.name
-  // }
-  
-  
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`
+      )
+      .then((response) => {
+        setWeather([response.data]);
+      });
+  }, [country.capital]);
 
-  // useEffect(async () => {
-  //     // You can await here
-  //     const response = await axios.get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`
-  //     );
-  //     // ...
-  //     setWeather(response);
-     
-  //   }
-  //   ,[weather]); // Or [] if effect doesn't need props or state
-  
+  if (weather.length === 0) {
+    return <p>Loading..</p>;
+  }
+
   return (
     <div>
-      <h1>{country.name}</h1>
+      }<h1>{country.name}</h1>
       <h3>capital: {country.capital}</h3>
       <h3>population: {country.population}</h3>
       <h2>Languages</h2>
@@ -32,7 +30,13 @@ const OneCountry = (props) => {
         })}
       </ul>
       <img src={country.flag} alt="Country flag" style={{ width: 300 }} />
-      <h2>Weather in {}</h2>
+      <h2>Weather in {weather[0].location.name}</h2>
+      <h4>Temperature: {weather[0].current.temperature}celsius</h4>
+      <img src={weather[0].current.weather_icons[0]} alt="weather icon" />
+      <h4>
+        Wind: {weather[0].current.wind_speed}km/h direction:{" "}
+        {weather[0].current.wind_dir}
+      </h4>
     </div>
   );
 };
