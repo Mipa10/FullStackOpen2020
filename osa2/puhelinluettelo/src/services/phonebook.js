885be932f7d48601c7a4a57e5ff8b62efa212902
Mipa <mikko.paivikko@gmail.com>
2020-10-04 19:56:19 +0300

    commit kakkonen

--- osa2/puhelinluettelo/src/services/phonebook.js
+++ osa2/puhelinluettelo/src/services/phonebook.js
@@ -1,28 +1,30 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
-  return axios.get(baseUrl).then(response => {
+  return axios.get(baseUrl).then((response) => {
     return response.data;
   });
 };
 
 const create = (newObject) => {
-    return axios.post(baseUrl,newObject).then(response => response.data)
-}
+  return axios.post(baseUrl, newObject).then((response) => response.data);
+};
 
-const del = person => {
-  return axios.delete(`${baseUrl}/${person.id}`)
-}
+const del = (person) => {
+  return axios.delete(`${baseUrl}/${person.id}`);
+};
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`,newObject).then(response => response.data)
-}
+  return axios
+    .put(`${baseUrl}/${id}`, newObject)
+    .then((response) => response.data);
+};
 
 export default {
   getAll,
   create,
   del,
-  update
+  update,
 };
