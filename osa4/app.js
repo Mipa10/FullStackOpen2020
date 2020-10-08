dd2bce8159325c9ac3d4ea76eb4d5456aacb5a34
Mipa <mikko.paivikko@gmail.com>
2020-10-08 19:49:10 +0300

    commit2

--- osa4/app.js
+++ osa4/app.js
@@ -1,42 +1,44 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
-  
-  const mongoUrl = config.MONGODB_URI
-  mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  
-  app.use(cors())
-  app.use(express.json())
-  
-  
-  app.get('/api/blogs', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  gsdgsdfggsdgdsfgdsfg
-  
-  app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
 
-  module.exports = app
+const mongoUrl = config.MONGODB_URI;
+logger.info("connecting to", mongoUrl);
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+      logger.error('error connecting to MongoDB', error.message)
+  });
+  
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.post("/", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+module.exports = app;
--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,41 +1,19 @@
-const blogsRouter = require('express').Router()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
+const blogsRouter = require("express").Router();
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
-  
-  const mongoUrl = config.MONGODB_URI
-  mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  
-  app.use(cors())
-  app.use(express.json())
-  
-  blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+const Blog = require("../models/blog");
 
-  module.exports = blogsRouter
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.post("/", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+module.exports = blogsRouter;
