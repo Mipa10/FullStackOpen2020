614c89ebd156fa6d8da60c1b97835f7ee719e8bb
Mipa <mikko.paivikko@gmail.com>
2020-10-09 23:49:08 +0300

    tehtävään 4.7 asti

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,3 +1,6 @@
+const _ = require('lodash')
+
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -12,7 +15,73 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+    const reducer = (most, current) => {
+        if(most.likes > current.likes){
+            return most
+            
+            
+        }
+        return current
+
+    }
+    let favorite = blogs.reduce(reducer,blogs[0])
+  
+    
+    favorite = {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    }
+    return favorite
+}
+
+const mostBlogs = (blogs) => {
+  
+  const inOrder = _.countBy(blogs, blog => blog.author)
+ 
+  var maxKey = _.max(Object.keys(inOrder))
+  var maxValue = _.max(Object.values(inOrder))
+
+  const response = {
+    author: maxKey,
+    blogs: maxValue
+  }
+  return response
+  
+}
+
+const mostLikes = (blogs) => {
+  const group = _.groupBy(blogs, blog => blog.author)
+  const sortedByAuthor = _.map(group, author => {
+    const likes = author.map( blog => {
+       return blog.likes
+     })
+     const reducer = (sum, item) => {
+       return sum + item
+     }
+     const total = likes.reduce(reducer,0)
+     return {
+       author: author[0].author,
+       likes:total
+      }
+  })
+  const reducer = (most, current) => {
+    if(most.likes > current.likes){
+        return most  
+    }
+    return current
+
+}
+let favorite = sortedByAuthor.reduce(reducer,sortedByAuthor[0])
+
+  return favorite
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 };
