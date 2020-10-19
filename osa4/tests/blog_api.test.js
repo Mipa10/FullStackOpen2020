32111da444560e78941613c24407328498a1b9e0
Mipa <mikko.paivikko@gmail.com>
2020-10-19 14:49:18 +0300

    4.10

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -47,6 +47,22 @@ describe('apitests', () => {
         expect(element.id).toBeDefined()
       });
     })
+    test('able to add one blog', async () => {
+      const newBlog = {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0,
+      }
+      await api.post('/api/blogs', newBlog)
+      
+ 
+      const response = await api.get('/api/blogs')
+      expect(response.body).toHaveLength(3)
+
+    })
 
 })
 
