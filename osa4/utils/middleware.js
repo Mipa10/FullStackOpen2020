9662d9f8c1300ec8fee8f30f6765ae513cc3a0c0
Mipa <mikko.paivikko@gmail.com>
2020-10-25 11:45:02 +0200

    4.20

--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -15,4 +15,13 @@ const errorHandler = (err, req, res, next) => {
     }
 }
 
-module.exports = {errorHandler, requestLogger}
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        request.token = authorization.substring(7)
+    }
+    next()
+
+}
+
+module.exports = {errorHandler, requestLogger, tokenExtractor}
